bda4077866f656f4d5ceade144a5b932eb5f21a6
nWacky <gosha.evtushenko@gmail.com>
2023-11-13 19:33:43 +0300

    map metrics to prefixed

--- src/runtime/handler.ts
+++ src/runtime/handler.ts
@@ -1,11 +1,29 @@
 import { defineEventHandler } from 'h3'
-import { register } from 'prom-client'
+
+import { AggregatorRegistry, register } from 'prom-client'
+
+import { useRuntimeConfig } from '#app'
 
 export default defineEventHandler(async (event) => {
   try {
-    const data = await register.metrics()
+    const params = useRuntimeConfig().public.prometheus
 
+    const allMetrics = await register.getMetricsAsJSON()
 
+    if (params.prefix) {
+      allMetrics.map((m) => {
+        return {
+          ...m,
+          name: `${params.prefix}${m.name}`,
+        }
+      })
+    }
+
+    const aggrRegistry = AggregatorRegistry.aggregate(allMetrics)
+
+    const data = await aggrRegistry.metrics()
+
+    console.log('got data:', data)
 
     event.res.setHeader('Content-Type', register.contentType)
     event.res.end(data)
