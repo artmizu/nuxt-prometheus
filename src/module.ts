e174560c9f607d2a5b3d3da9a345f57cbca383f8
nWacky <gosha.evtushenko@gmail.com>
2023-11-14 13:36:10 +0300

    clean up

--- src/module.ts
+++ src/module.ts
@@ -1,5 +1,5 @@
 import { defu } from 'defu'
-import { addPlugin, addServerHandler, addServerPlugin, createResolver, defineNuxtModule } from '@nuxt/kit'
+import { addPlugin, addServerHandler, createResolver, defineNuxtModule } from '@nuxt/kit'
 import type { NuxtModule } from '@nuxt/schema'
 import { name, version } from '../package.json'
 import type { AnalyticsModuleParams } from './runtime/type'
