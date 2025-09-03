32b92783005300aab3f8d9f5802c5d01b708527a
artmizu <hello@artmizu.ru>
2025-09-03 23:58:48 +0900

    nuxt 4 version support and small test fixes

--- k6/load.test.js
+++ k6/load.test.js
@@ -5,7 +5,7 @@ import globalOptions from './options.js'
 export const options = globalOptions
 
 export default function () {
-  const res = http.get('http://192.168.1.17:3000/memory-measure')
+  const res = http.get('http://192.168.0.220:3000/memory-measure')
   check(res, {
     'is status 200': r => r.status === 200,
     'is contain elements': r => r.html('#response'),
